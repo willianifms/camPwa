1d159d17f143ba8203283976ae7a5a583ef635eb
willianifms <williancardoso00004@gmail.com>
2023-10-30 17:07:18 -0400

    add att

--- js/main.js
+++ js/main.js
@@ -1,43 +1,54 @@
-//registrando a service worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', async () => {
-    try {
-    let reg;
-    reg = await navigator.serviceWorker.register('/sw.js', { type: "module" });
-    console.log('Service worker registrada! ', reg); } catch (err) {
-    console.log(" Service worker registro falhou: ", err);
-    }
+        try {
+            let reg;
+            reg = await navigator.serviceWorker.register('/sw.js', { type: "module" });
+            console.log('Service worker registrada! ', reg);
+        } catch (err) {
+            console.log(" Service worker registro falhou: ", err);
+        }
     });
-    }
-    // configurando as constraintes do video stream
-    var constraints = { video: { facingMode: "user" }, audio: false };
-    // capturando os elementos em tela
-    const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")
-
-  
-
-//Estabelecendo o acesso a camera e inicializando a visualização
-function cameraStart() {
-navigator.mediaDevices
-.getUserMedia(constraints)
-.then(function (stream) {
-let track = stream.getTracks()[0]; cameraView.srcObject = stream;
-})
-.catch(function (error) {
-console.error("Ocorreu um Erro.", error);
-});
 }
-// Função para tirar foto
 
-cameraTrigger.onclick= function () {
+var constraints = { video: { facingMode: "user" }, audio: false };
+
+const cameraView = document.querySelector("#camera--view");
+const cameraOutput = document.querySelector("#camera--output");
+const cameraSensor = document.querySelector("#camera--sensor");
+const cameraTrigger = document.querySelector("#camera--trigger");
+const lastPhotos = document.querySelectorAll(".last-photos");
+
+let lastThreePhotos = [];
+
+function cameraStart() {
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(function (stream) {
+            let track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function (error) {
+            console.error("Ocorreu um Erro.", error);
+        });
+}
+
+cameraTrigger.onclick = function () {
     cameraSensor.width = cameraView.videoWidth;
-cameraSensor.height = cameraView.videoHeight;
-cameraSensor.getContext("2d").drawImage(cameraView, 0, 0); 
-cameraOutput.src = cameraSensor.toDataURL("image/webp");
-cameraOutput.classList.add("taken");
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    const data = cameraSensor.toDataURL("image/webp");
+
+    if (lastThreePhotos.length === 3) {
+        lastThreePhotos.shift();
+    }
+    lastThreePhotos.push(data);
+
+    for (let i = 0; i < lastThreePhotos.length; i++) {
+        lastPhotos[i].src = lastThreePhotos[i];
+        lastPhotos[i].classList.add("taken");
+    }
+
+    cameraOutput.src = data;
+    cameraOutput.classList.add("taken");
 };
-// carrega imagem de camera quando a janela carregar window.addEventListener("load", cameraStart, false);
-    window.addEventListener("load", cameraStart, false)
+
+window.addEventListener("load", cameraStart, false);
