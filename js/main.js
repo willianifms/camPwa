154b89befe411b0b658db6f1b80b707d7667f5d3
willianifms <williancardoso00004@gmail.com>
2023-10-30 17:26:44 -0400

    sobe

--- js/main.js
+++ js/main.js
@@ -1,54 +1,60 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', async () => {
-        try {
-            let reg;
-            reg = await navigator.serviceWorker.register('/sw.js', { type: "module" });
-            console.log('Service worker registrada! ', reg);
-        } catch (err) {
-            console.log(" Service worker registro falhou: ", err);
-        }
+      try {
+        let reg;
+        reg = await navigator.serviceWorker.register('./sw.js', {type: "module"});
+        console.log('Service worker registrada! reg');
+      } catch (err) {
+        console.log(' Service worker registro falhou:', err);
+      }
     });
-}
-
-var constraints = { video: { facingMode: "user" }, audio: false };
-
-const cameraView = document.querySelector("#camera--view");
-const cameraOutput = document.querySelector("#camera--output");
-const cameraSensor = document.querySelector("#camera--sensor");
-const cameraTrigger = document.querySelector("#camera--trigger");
-const lastPhotos = document.querySelectorAll(".last-photos");
-
-let lastThreePhotos = [];
-
-function cameraStart() {
-    navigator.mediaDevices.getUserMedia(constraints)
-        .then(function (stream) {
-            let track = stream.getTracks()[0];
-            cameraView.srcObject = stream;
-        })
-        .catch(function (error) {
-            console.error("Ocorreu um Erro.", error);
-        });
-}
-
-cameraTrigger.onclick = function () {
+  };
+  
+  var constraints = {video: {facingMode: "user"}, audio: false };
+  
+  const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")
+  
+  const fotos = []
+  
+  function cameraStart(){
+    navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function (stream) {
+      let track = stream.getTracks()[0];
+      cameraView.srcObject = stream;
+    })
+    .catch(function (error) {
+      console.error("Ocorreu um erro.", error)
+    });
+  };
+  
+  cameraTrigger.onclick = function () {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    const data = cameraSensor.toDataURL("image/webp");
-
-    if (lastThreePhotos.length === 3) {
-        lastThreePhotos.shift();
-    }
-    lastThreePhotos.push(data);
-
-    for (let i = 0; i < lastThreePhotos.length; i++) {
-        lastPhotos[i].src = lastThreePhotos[i];
-        lastPhotos[i].classList.add("taken");
-    }
-
-    cameraOutput.src = data;
+    const url = cameraSensor.toDataURL("image/webp");
+    cameraOutput.src = url
     cameraOutput.classList.add("taken");
-};
-
-window.addEventListener("load", cameraStart, false);
+  
+    if (fotos.length >= 3) {
+      fotos.shift(); // Remove a foto mais antiga se houver 3 ou mais fotos na lista.
+    }
+    fotos.push(url);
+    displayFotos();
+  };
+  
+  function displayFotos () {
+    const fotosContainer = document.querySelector("#fotos-container");
+    fotosContainer.innerHTML = ""; // Limpa o conteúdo anterior.
+  
+    fotos.forEach((url) => {
+      const img = document.createElement("img");
+      img.src = url;
+      fotosContainer.appendChild(img);
+    });
+  }
+  
+  window.addEventListener("load", cameraStart, false);
